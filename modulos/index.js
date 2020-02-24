53982c4c4c61c1000d778ee21982e38446546453
Matheus H. de Souza <mh.matheussouza@gmail.com>
2020-02-24 16:08:16 -0300

    Módulo de como modularizar aplicações

---
+++ modulos/index.js
@@ -0,0 +1,7 @@
+// import comprimento from "./circunferencia";
+// import * as circunferencia from "./circunferencia";
+import {comprimento, area, PI} from "./circunferencia";
+
+console.log('comprimento: ', comprimento(2));
+console.log('area: ', area(2));
+console.log('PI: ', PI);
